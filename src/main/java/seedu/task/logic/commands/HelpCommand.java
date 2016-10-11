7cb730f0aab06abc3118ab10626f48c8ea9475e2
Poh Yee Heng <a0125534@u.nus.edu>
2016-10-11 13:33:35 +0800

    Added help functions to list all the task book commands

--- src/main/java/seedu/task/logic/commands/HelpCommand.java
+++ src/main/java/seedu/task/logic/commands/HelpCommand.java
@@ -1,26 +1,31 @@
 package seedu.task.logic.commands;
 
-
-import seedu.task.commons.events.ui.ShowHelpEvent;
-import seedu.taskcommons.core.EventsCenter;
-
 /**
  * Format full help instructions for every command for display.
  */
 public class HelpCommand extends Command {
 
-    public static final String COMMAND_WORD = "help";
+	public static final String COMMAND_WORD = "help";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
-            + "Example: " + COMMAND_WORD;
+	public static final String MESSAGE_USAGE = "Opened HELP panel.\n\n" 
+			+ "Parameters: help [KEY_WORD]\n" + "Example: "
+			+ COMMAND_WORD + " add\n" 
+			+ "List of available commands\n" 
+			+ AddTaskCommand.MESSAGE_USAGE + "\n\n"
+			+ DeleteCommand.MESSAGE_USAGE + "\n\n"
+			+ FindCommand.MESSAGE_USAGE + "\n\n"
+			+ ListCommand.MESSAGE_USAGE + "\n\n" 
+			+ SelectCommand.MESSAGE_USAGE + "\n\n" 
+			+ "Clear: Clear the TaskBook\n" + "Example: clear\n\n" 
+			+ "Exit : Close the program\n" + "Example: Exit";
+	
+	public HelpCommand() {
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+	}
 
-    public HelpCommand() {}
+	@Override
+	public CommandResult execute() {
 
-    @Override
-    public CommandResult execute() {
-        EventsCenter.getInstance().post(new ShowHelpEvent());
-        return new CommandResult(SHOWING_HELP_MESSAGE);
-    }
+			return new CommandResult(MESSAGE_USAGE);
+	}
 }
