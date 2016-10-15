f6e9670151b442201ee4f0f03560b5b105feb67f
githubxxcc <xuchen727@hotmail.com>
2016-10-15 12:27:09 +0800

    Switched to OOP

---
+++ src/test/java/seedu/task/logic/MarkCommandTest.java
@@ -0,0 +1,51 @@
+package seedu.task.logic;
+
+import static seedu.taskcommons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import seedu.task.logic.TestDataHelper;
+import seedu.task.logic.commands.MarkCommand;
+import seedu.task.model.TaskBook;
+import seedu.task.model.item.Task;
+
+
+
+
+public class MarkCommandTest extends CommandTest{
+
+    @Test
+    public void execute_MarkInvalidArgsFormat_errorMessageShown() throws Exception {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, MarkCommand.MESSAGE_USAGE);
+        assertIncorrectIndexFormatBehaviorForCommand("mark", expectedMessage);
+    }
+    
+    @Test
+    public void execute_MarkIndexNotFound_errorMessageShown() throws Exception {
+        assertTaskIndexNotFoundBehaviorForCommand("mark");
+    }
+    
+    @Test
+    public void execute_mark_marksCorrectTask() throws Exception {
+        TestDataHelper helper = new TestDataHelper();
+        
+        Task t1 = helper.generateTask(1);
+        Task t2 = helper.generateTask(2);
+        Task t3 = helper.generateTask(3);
+        
+        List<Task> threeTasks = helper.generateTaskList(3);
+        List<Task> expectedABList = helper.generateTaskList(3);
+        List<Task> expectedList = helper.generateTaskList(t1, t3);
+        helper.addTaskToModel(model, threeTasks);
+
+        TaskBook expectedAB = helper.generateTaskBook_Tasks(expectedABList);
+        expectedAB.markTask(t2);
+
+
+        assertTaskCommandBehavior("mark 2",
+                String.format(MarkCommand.MESSAGE_MARK_TASK_SUCCESS, 2),
+                expectedAB,expectedList);
+    }
+}
