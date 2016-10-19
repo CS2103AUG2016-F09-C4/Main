f47c863cb1342e0053f78c1dcc43cee171070360
Poh Yee Heng <a0125534@u.nus.edu>
2016-10-19 16:30:33 +0800

    Reupload pull request on Implement_Help_Command

--- src/main/java/seedu/task/logic/commands/ClearCommand.java
+++ src/main/java/seedu/task/logic/commands/ClearCommand.java
@@ -7,8 +7,39 @@ import seedu.task.model.TaskBook;
  */
 public class ClearCommand extends Command {
 
+<<<<<<< Updated upstream
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+=======
+	public static final String COMMAND_WORD = "clear";
+	public static final String MESSAGE_USAGE = COMMAND_WORD + "\n"
+			+ "Clear tasks/events in Taskbook.\n\n"
+    		+ "Clears completed tasks and events\n" 
+			+ "Example:" + COMMAND_WORD +"\n\n"
+			
+    		+ "Clears completed tasks.\n" 
+			+ "Parameters: CLEAR_TYPE]\n" 
+    		+ "Example: " + COMMAND_WORD + " -t\n\n" 
+    		
+    		+ "Clears completed events.\n" 
+    		+ "Parameters: CLEAR_TYPE]\n" 
+    		+ "Example: " + COMMAND_WORD + " -e\n\n"
+    		
+    		+ "Clears all completed and uncompleted tasks.\n" 
+    		+ "Optional flag: [-a] to request show all tasks" 
+    		+ "Parameters: CLEAR_TYPE + Optional flag \n" 
+    		+ "Example: " + COMMAND_WORD + " -t -a\n\n"
+    		
+    		+ "Clears all completed and uncompleted events.\n" 
+    		+ "Optional flag: [-a] to request show all events" 
+    		+ "Parameters: CLEAR_TYPE + Optional flag \n" 
+    		+ "Example: " + COMMAND_WORD + " -e -a\n\n"
+    		
+    		+ "Clears all completed and uncompleted tasks and events.\n" 
+    		+ "Optional flag: [-a] to request show all tasks and events"  
+    		+ "Example: " + COMMAND_WORD + "-a\n\n";
+	
+>>>>>>> Stashed changes
 
     public ClearCommand() {}
 
--- src/main/java/seedu/task/logic/commands/HelpCommand.java
+++ src/main/java/seedu/task/logic/commands/HelpCommand.java
@@ -8,6 +8,39 @@ import seedu.taskcommons.core.EventsCenter;
  * Format full help instructions for every command for display.
  */
 public class HelpCommand extends Command {
+<<<<<<< Updated upstream
+=======
+	public final String commandWord;
+	public boolean isPopUp;
+	public static final String COMMAND_WORD = "help";
+
+	public static final String MESSAGE_USAGE = COMMAND_WORD + "\n"
+			+ "Shows program commands usage instructions.\n" 
+			+ "Example: " + COMMAND_WORD + "\n\n"  
+			+ "List of available commands for help\n" + COMMAND_WORD + " add\n" + COMMAND_WORD + " delete\n"
+			+ COMMAND_WORD + " find\n" + COMMAND_WORD + " list\n" + COMMAND_WORD + " select\n" + COMMAND_WORD
+			+ " mark\n" + COMMAND_WORD + " clear\n" + COMMAND_WORD + " exit"
+			+ "Parameters: help [KEY_WORD]\n"
+			+ "Example: "+COMMAND_WORD+"add\n\n";
+	
+
+	public HelpCommand(String commandWord, boolean helpWindowPopUp) {
+		// TODO Auto-generated constructor stub
+		this.commandWord = commandWord;
+		this.isPopUp = helpWindowPopUp;
+	}
+
+	@Override
+	public CommandResult execute() {
+
+		if (isPopUp == true) {
+			EventsCenter.getInstance().post(new ShowHelpEvent());
+			return new CommandResult(commandWord);
+		} else {
+			return new CommandResult(commandWord);
+		}
+	}
+>>>>>>> Stashed changes
 
     public static final String COMMAND_WORD = "help";
 
