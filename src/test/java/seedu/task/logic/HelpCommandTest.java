88f96b31a748f68c0c2d4575f3a0db5d10925a4a
Poh Yee Heng <a0125534@u.nus.edu>
2016-10-20 18:43:47 +0800

    Added heuristic test for help command

--- src/test/java/seedu/task/logic/HelpCommandTest.java
+++ src/test/java/seedu/task/logic/HelpCommandTest.java
@@ -2,32 +2,128 @@ package seedu.task.logic;
 
 import static org.junit.Assert.assertTrue;
 import static seedu.taskcommons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.taskcommons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import seedu.task.logic.commands.AddCommand;
+import seedu.task.logic.commands.ClearCommand;
+import seedu.task.logic.commands.DeleteCommand;
+import seedu.task.logic.commands.EditCommand;
+import seedu.task.logic.commands.ExitCommand;
+import seedu.task.logic.commands.FindCommand;
 import seedu.task.logic.commands.HelpCommand;
+import seedu.task.logic.commands.ListCommand;
+import seedu.task.logic.commands.MarkCommand;
 
 public class HelpCommandTest extends CommandTest {
-    /******************************Pre and Post set up*****************************/
-    @Before
-    public void setup() {
-        super.setup();
-    }
+	/******************************
+	 * Pre and Post set up
+	 *****************************/
+	@Before
+	public void setup() {
+		super.setup();
+	}
 
-    @After
-    public void teardown() {
-        super.teardown();
-    }
-    
-    /************************************Test cases*****************************/
-    
-    @Test
-    public void execute_help() throws Exception {
-        assertHelpCommandBehavior("help", HelpCommand.MESSAGE_USAGE);
-        assertTrue(helpShown);
-    }
-    
+	@After
+	public void teardown() {
+		super.teardown();
+	}
+
+	/************************************
+	 * Test cases
+	 *****************************/
+
+	/*
+	 * Tests for executing help command
+	 */
+
+	/*
+	 * 1) Invalid help parameters 
+	 * - Null 
+	 * - Unknown [KEY_WORD]
+	 */
+	
+	@Test
+	public void Help() throws Exception {
+		
+		assertCommandBehavior("help", HelpCommand.MESSAGE_USAGE);
+	}
+
+	@Test
+	public void Null_Help_Parameters() throws Exception {
+		String invalidCommand = "       ";
+		assertCommandBehavior(invalidCommand, String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+	}
+
+	@Test
+	public void execute_unknownCommandWord() throws Exception {
+		String unknownCommand = "uicfhmowqewca";
+		assertCommandBehavior(unknownCommand,String.format(MESSAGE_UNKNOWN_COMMAND, HelpCommand.MESSAGE_USAGE));
+	}
+	
+	
+	/*
+	 * 1) valid help parameters 
+	 * - add
+	 * - delete
+	 * - list 
+	 * - find 
+	 * - edit 
+	 * - mark 
+	 * - clear 
+	 * - exit 
+	 */
+	
+	@Test
+	public void execute_addCommand() throws Exception {
+		String addCommand = "add";
+		assertCommandBehavior(addCommand, AddCommand.MESSAGE_USAGE);
+	}
+	
+	@Test
+	public void execute_deleteCommand() throws Exception {
+		String deleteCommand = "delete";
+		assertCommandBehavior(deleteCommand, DeleteCommand.MESSAGE_USAGE);
+	}
+	
+	@Test
+	public void execute_listCommand() throws Exception {
+		String listCommand = "list";
+		assertCommandBehavior(listCommand, ListCommand.MESSAGE_USAGE);
+	}
+	
+	@Test
+	public void execute_findCommand() throws Exception {
+		String findCommand = "find";
+		assertCommandBehavior(findCommand, FindCommand.MESSAGE_USAGE);
+	}
+	
+	@Test
+	public void execute_editCommand() throws Exception {
+		String editCommand = "edit";
+		assertCommandBehavior(editCommand, EditCommand.MESSAGE_USAGE);
+	}
+	
+	@Test
+	public void execute_markCommand() throws Exception {
+		String markCommand = "mark";
+		assertCommandBehavior(markCommand, MarkCommand.MESSAGE_USAGE);
+	}
+	
+	@Test
+	public void execute_clearCommand() throws Exception {
+		String clearCommand = "clear";
+		assertCommandBehavior(clearCommand, ClearCommand.MESSAGE_USAGE);
+	}
+	
+	@Test
+	public void execute_exitCommand() throws Exception {
+		String exitCommand = "exit";
+		assertCommandBehavior(exitCommand, ExitCommand.MESSAGE_USAGE);
+	}
+	
 }
