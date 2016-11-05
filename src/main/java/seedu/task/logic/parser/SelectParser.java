8886853cece34f9569498fee7262e052135f0bfc
Poh Yee Heng <a0125534@u.nus.edu>
2016-11-05 23:46:06 +0800

    Revert "To improve Codacy"
    This reverts commit 39e3065794645cf5bf09569508522091e3af6148.

--- src/main/java/seedu/task/logic/parser/SelectParser.java
+++ src/main/java/seedu/task/logic/parser/SelectParser.java
@@ -17,6 +17,10 @@ import seedu.task.logic.commands.SelectTaskCommand;
  */
 
 public class SelectParser implements Parser {
+
+	public SelectParser() {
+	}
+
 	// '/' forward slashes are reserved for delimiter prefixes
 	private static final Pattern SELECT_TASK_DATA_FORMAT = Pattern.compile("(?:/t)\\s(?<index>\\d*)");
 
